b07501983ea86bf77f269a957f4dd694ae7d97c8
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-30 21:42:28 -0400

    feat: theme definition and global styles

---
+++ App/styles/appTheme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "react-native-paper";
+
+export const appTheme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#b5c401",
+    accent: "#f1c40f",
+  },
+};
---
+++ App/styles/global.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+
+export const globalStyles = StyleSheet.create({
+  textInput: {
+    backgroundColor: "white",
+    width: "100%",
+    marginHorizontal: 0,
+    marginVertical: 5,
+    paddingHorizontal: 16,
+  },
+});
+
+
