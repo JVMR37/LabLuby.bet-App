a879ceb0fe5c34dbd5bfa35791e4e7d677bacae6
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-30 21:43:44 -0400

    feat: Add HomePage and SignupPage files

---
+++ App/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+const HomePage: React.FC = () => {
+    //TODO: Implement this
+  return <View></View>;
+};
+
+export default HomePage;
---
+++ App/pages/SignupPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+const SignupPage: React.FC = () => {
+  //TODO: Implement this
+
+  return <View></View>;
+};
+
+export default SignupPage;
