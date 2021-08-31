35590eab09684fe72642c0a3d70458c9c1f3bdce
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-30 21:34:24 -0400

    feat: definidindo estrutura de navegação

--- App.tsx
+++ App.tsx
@@ -1,22 +1,38 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import LogoComponent from "./App/components/LogoComponent";
+import { Provider as PaperProvider } from "react-native-paper";
+import { appTheme } from "./App/styles/appTheme";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginPage from "./App/pages/LoginPage";
+import HomePage from "./App/pages/HomePage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [pseudoToken, setPseudoToken] = React.useState("");
+
   return (
-    <View style={styles.container}>
-      <LogoComponent />
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={appTheme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {!pseudoToken ? (
+            // No token found, user isn't signed in
+            <Stack.Screen
+              name="SignIn"
+              component={LoginPage}
+              options={{
+                title: "",
+                headerShown: false,
+                animationTypeForReplace: pseudoToken ? "pop" : "push",
+              }}
+            />
+          ) : (
+            // User is signed in
+            <Stack.Screen name="Home" component={HomePage} />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
