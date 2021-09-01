4a22ca9b9373b985d35e302c1ec48f943b1cba1f
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-31 20:22:59 -0400

    feat: Definindo telas de ResetPasss e de Singup para navegação

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginPage from "./App/pages/LoginPage";
+import SignupPage from "./App/pages/SignupPage";
+import ResetPasswordPage from "./App/pages/ResetPasswordPage";
 import HomePage from "./App/pages/HomePage";
 
 const Stack = createNativeStackNavigator();
@@ -18,15 +20,34 @@ export default function App() {
         <Stack.Navigator>
           {!pseudoToken ? (
             // No token found, user isn't signed in
-            <Stack.Screen
-              name="SignIn"
-              component={LoginPage}
-              options={{
-                title: "",
-                headerShown: false,
-                animationTypeForReplace: pseudoToken ? "pop" : "push",
-              }}
-            />
+            <>
+              <Stack.Screen
+                name="SignIn"
+                component={LoginPage}
+                options={{
+                  headerShown: false,
+                  animationTypeForReplace: pseudoToken ? "pop" : "push",
+                }}
+              />
+
+              <Stack.Screen
+                name="Signup"
+                component={SignupPage}
+                options={{
+                  headerShown: false,
+                  animationTypeForReplace: pseudoToken ? "pop" : "push",
+                }}
+              />
+
+              <Stack.Screen
+                name="ResetPassword"
+                component={ResetPasswordPage}
+                options={{
+                  headerShown: false,
+                  animationTypeForReplace: pseudoToken ? "pop" : "push",
+                }}
+              />
+            </>
           ) : (
             // User is signed in
             <Stack.Screen name="Home" component={HomePage} />
