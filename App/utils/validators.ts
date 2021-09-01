79d508aff0542fbcbca0cd1948ec4bf6c2e327ca
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-31 20:28:42 -0400

    feat: definido validators para inputs

---
+++ App/utils/validators.ts
@@ -0,0 +1,11 @@
+export const emailValidator = (text: string) => {
+  const emailValitionRegex = /^[\w\+\.]+@\w+\.\w{2,253}(?:\.?)[\w]{0,2}$/;
+  return emailValitionRegex.test(text);
+};
+
+export const passValidator = (text: string) => text.trim().length >= 6;
+
+export const confirmPassValidator = (text: string, otherText: string) =>
+  text === otherText;
+
+export const isNotEmptyValidator = (text: string) => text.trim() !== "";
