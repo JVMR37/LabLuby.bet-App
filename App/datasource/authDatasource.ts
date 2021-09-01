df1f22bc4151cd510a8bf9a6bf26190856515c66
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-01 12:07:36 -0400

    feat: datasource implementation

---
+++ App/datasource/authDatasource.ts
@@ -0,0 +1,138 @@
+import User from "../models/User";
+import ApiDatasource from "./apiDatasource";
+
+export const loginInAPI = async (email: string, password: string) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    apiUtils.Axios.post("/sessions", {
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        console.log(response);
+
+        apiUtils.setToken(response.data.token);
+
+        resolve({
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
+};
+
+export const registerUserInDB = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    apiUtils.Axios.post("/users", {
+      name: name,
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        console.log(response);
+
+        resolve({
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
+};
+
+export const updateUserDataInDB = async (
+  userId: string,
+  name: string,
+  email: string
+) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    apiUtils.Axios.put(`/users/${userId}`, {
+      name: name,
+      email: email,
+    })
+      .then((response) => {
+        console.log(response);
+
+        resolve({
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
+};
+
+export const requestPasswordChange = async (email: string) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    apiUtils.Axios.post("/passwords", {
+      email: email,
+      redirect_url: "http://localhost:3000/update-password",
+    })
+      .then((response) => {
+        console.log(response);
+
+        apiUtils.setToken(response.data.token);
+
+        resolve({
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
+};
+
+export const sendNewPassword = async (
+  token: string,
+  password: string,
+  password_confirmation: string
+) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+
+    apiUtils.Axios.put("/passwords", {
+      token,
+      password,
+      password_confirmation,
+    })
+      .then((response) => {
+        console.log(response);
+
+        apiUtils.setToken(response.data.token);
+
+        resolve({
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
+};
---
+++ App/datasource/gameDatasource.ts
@@ -0,0 +1,71 @@
+import { AxiosResponse } from "axios";
+import Game from "../models/Game";
+import SavedGame from "../models/SavedGame";
+import ApiDatasource from "./apiDatasource";
+import PaginationMetadata from "../models/PaginationMetadata";
+
+export const fetchGames = async () => {
+  return new Promise<{ data: Game[] }>((resolve, reject) => {
+    ApiDatasource.Instance.Axios.get<Game[]>("/types")
+      .then((response) => {
+        console.log(response.data);
+        resolve({ data: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  }).catch((error) => {
+    return Error(error.message);
+  });
+};
+
+export const fetchSavedBets = async (page: number = 1, filter?: string) => {
+  return new Promise<{ data: SavedGame[]; meta: PaginationMetadata }>(
+    (resolve, reject) => {
+      ApiDatasource.Instance.Axios.get(
+        `/bets?page=${page}${filter ? "&filter=" + filter : ""}`
+      )
+        .then((response) => {
+          console.log(response.data);
+          const responseData = response.data.data.map((betsJson: any) =>
+            SavedGame.fromJson(betsJson)
+          );
+          resolve({
+            data: responseData,
+            meta: PaginationMetadata.fromJson(response.data.meta),
+          });
+        })
+        .catch((error) => {
+          if (error.messages) {
+            console.log(error.messages);
+          } else {
+            console.log(error);
+          }
+          reject(error);
+        });
+    }
+  ).catch((error) => {
+    return Error(error.message);
+  });
+};
+
+export const saveBetsInDB = async (
+  bets: SavedGame[]
+): Promise<AxiosResponse | Error> => {
+  return new Promise<AxiosResponse | Error>((resolve, reject) => {
+    ApiDatasource.Instance.Axios.post("/bets", {
+      bets: bets.map((bet) => bet.toJson()),
+    })
+      .then((response) => {
+        console.log(response.data);
+        resolve(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  }).catch((error) => {
+    return Error(error.message);
+  });
+};
