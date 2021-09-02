7bbaa11cbfa03d2c6395b34013e7121e7dd85686
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-02 10:16:11 -0400

    feat: Definição do componenete base do app

---
+++ App/layout/AppScaffold.tsx
@@ -0,0 +1,64 @@
+import React, { Fragment } from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, StyleSheet } from "react-native";
+import LogoComponent from "../components/LogoComponent";
+import { IconButton, Appbar } from "react-native-paper";
+import { useAppDispatch } from "../hooks/hooks";
+import { useMountEffect } from "../hooks/use-mount-effect";
+import { logout } from "../store/authSlice";
+import { loadGames, loadSavedBets } from "../store/gamesSlice";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const AppScaffold: React.FC = (props) => {
+  const dispatch = useAppDispatch();
+
+  useMountEffect(() => {
+    dispatch(loadGames());
+    dispatch(loadSavedBets({ page: 1 }));
+  });
+
+  const logoutButtonHandler = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <SafeAreaView style={styles.container} key={"AppScaffold"}>
+      <StatusBar style={"auto"} backgroundColor={"white"} />
+      <View
+        style={{
+          ...styles.row,
+          ...styles.appBar,
+        }}
+      >
+        <LogoComponent fontSize={28} />
+        <IconButton icon="logout" onPress={logoutButtonHandler}></IconButton>
+      </View>
+      <View style={styles.appContent}>{props.children}</View>
+      <View style={styles.row}></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // marginTop: StatusBar.currentHeight,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  appBar: {
+    paddingVertical: 5,
+    paddingHorizontal: 16,
+  },
+  appContent: {
+    marginTop: 20,
+    marginBottom: 32,
+    marginHorizontal: 16,
+  },
+});
+
+export default AppScaffold;
