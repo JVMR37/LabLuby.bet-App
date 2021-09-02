5b8d2564f0432685d4e6802da8220593bf5321a4
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-02 10:16:45 -0400

    feat: Adicionando botões de filtro e sua lógica na HomePage

--- App/pages/HomePage.tsx
+++ App/pages/HomePage.tsx
@@ -1,9 +1,68 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useCallback } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import AppScaffold from "../layout/AppScaffold";
+import TitlePageComponent from "../components/TitlePageComponent";
+import { globalStyles } from "../styles/global.style";
+import GameOutlineButton from "../components/GameOutlineButton";
+import { useAppSelector, useAppDispatch } from "../hooks/hooks";
+import {
+  loadSavedBets,
+  selectAvailableGames,
+  selectFilter,
+  getGameFilter,
+} from "../store/gamesSlice";
+import Game from "../models/Game";
 
 const HomePage: React.FC = () => {
-    //TODO: Implement this
-  return <View></View>;
+  const dispatch = useAppDispatch();
+  const games = useAppSelector(selectAvailableGames) as Array<Game>;
+  const filter = useAppSelector(getGameFilter);
+  const filterButtonHandler = useCallback(
+    async (typeId: any) => {
+      console.log(typeId);
+
+      if (filter === typeId) {
+        dispatch(selectFilter(""));
+        dispatch(loadSavedBets({ page: 1 }));
+      } else {
+        dispatch(loadSavedBets({ page: 1, filter: typeId.toString() }));
+
+        dispatch(selectFilter(typeId));
+      }
+    },
+    [dispatch, filter]
+  );
+
+  const filterButtonsElements = useCallback(
+    () =>
+      games.map((game: Game) => (
+        <GameOutlineButton
+          key={game.id}
+          title={game.type}
+          isSelected={game.id.toString() == filter}
+          gameColor={game.color}
+          onPress={filterButtonHandler.bind(null, game.id)}
+        ></GameOutlineButton>
+      )),
+    [filter, filterButtonHandler, games]
+  )();
+
+  return (
+    <AppScaffold>
+      <Text style={{ ...globalStyles.titlePage }}>Recent Games</Text>
+      <Text>Filters</Text>
+      <View style={styles.filterButtonsRow}>{filterButtonsElements}</View>
+    </AppScaffold>
+  );
 };
+
+const styles = StyleSheet.create({
+  filterButtonsRow: {
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
 
 export default HomePage;
