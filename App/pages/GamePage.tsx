5e2a088255d1af6cf12bfb8ab349d7a8b18d3198
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-06 22:10:53 -0400

    feat: completing the features of the games page

--- App/pages/GamePage.tsx
+++ App/pages/GamePage.tsx
@@ -1,8 +1,7 @@
-import React, { useCallback } from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import React, { useCallback, useMemo, Fragment, useState } from "react";
+import { ScrollView, StyleSheet } from "react-native";
 import { globalStyles } from "../styles/global.style";
-
-import AppScaffold from "../layout/AppScaffold";
+import { Text } from "react-native-paper";
 import PageContainer from "../layout/PageContainer";
 import GameOutlineButton from "../components/GameOutlineButton";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
@@ -16,6 +15,7 @@ import {
 import Game from "../models/Game";
 import SelectedNumbers from "../components/SelectedNumbers";
 import GameActions from "../components/GameActions";
+import GameNumbersContainer from "../components/GameNumbersContainer";
 
 const GamePage: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -32,6 +32,38 @@ const GamePage: React.FC = () => {
     [dispatch]
   );
 
+  const descriptionOrGameActions = useMemo(() => {
+    return selectedNumbers.length > 0 ? (
+      <Fragment>
+        <SelectedNumbers
+          numbers={selectedNumbers}
+          gameColor={selectedGame!.color}
+        />
+        <GameActions />
+      </Fragment>
+    ) : (
+      <Fragment>
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontStyle: "italic",
+          }}
+        >
+          Fill your bet
+        </Text>
+
+        <Text
+          key={selectedGame!.type}
+          style={{
+            fontStyle: "italic",
+          }}
+        >
+          {selectedGame!.description}
+        </Text>
+      </Fragment>
+    );
+  }, [selectedNumbers, selectedGame]);
+
   const gameButtonsElements = useCallback(() => {
     return games.map((game: Game) => (
       <GameOutlineButton
@@ -42,31 +74,34 @@ const GamePage: React.FC = () => {
         onPress={gameButtonHandler.bind(this, game.type)}
       />
     ));
-  }, [games])();
+  }, [games, selectedGame])();
 
   return (
-    <PageContainer>
-      <Text style={globalStyles.titlePage}>New Bet for {""}</Text>
-      <Text>Choose a game</Text>
-      <ScrollView
-        style={{ minWidth: "100%", minHeight: 70 }}
-        contentContainerStyle={styles.gameButtonsRow}
-        horizontal
-      >
-        {gameButtonsElements}
-      </ScrollView>
+    <>
+      <PageContainer needsScroll={false}>
+        <Text style={globalStyles.titlePage}>
+          New Bet for {selectedGame!.type}
+        </Text>
+        <Text
+          style={{
+            fontStyle: "italic",
+            marginTop: 8,
+          }}
+        >
+          Choose a game
+        </Text>
+        <ScrollView
+          style={{ minWidth: "100%", minHeight: 70 }}
+          contentContainerStyle={styles.gameButtonsRow}
+          horizontal
+        >
+          {gameButtonsElements}
+        </ScrollView>
+        {descriptionOrGameActions}
 
-      <Text>Fill your bet</Text>
-
-      <Text>{"TODO: Description here"}</Text>
-
-      {selectedNumbers.length > 0 && (
-        <SelectedNumbers
-          numbers={selectedNumbers}
-          gameColor={selectedGame!.color}
-        />
-      )}
-    </PageContainer>
+        <GameNumbersContainer game={selectedGame!} />
+      </PageContainer>
+    </>
   );
 };
 
